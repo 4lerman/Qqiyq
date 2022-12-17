a7ac2cb2856b53a596cc39bbfda573c13c75990b
4lerman <almaserbolat60@gmail.com>
2022-12-17 13:45:21 +0600

    Initial Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout.pug");
+app.use(express.static("public"));
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+})
+	.then("open", () => console.log("Connected to MongoDB"))
+	.catch((err) => {
+		console.log(`Error occured: ${err.stack}`);
+		process.exit(1);
+	});
+
+app.use("/", indexRouter);
+app.listen(process.env.PORT || 3000);
